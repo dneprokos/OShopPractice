47a55ba71af49c6bae168a05c450d7419a73e395
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-06-02 16:41:32 +0300

    Last shopping cart feature refactoring before push

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,51 +3,60 @@ import { ProductService } from '../product.service';
 import { Product } from '../models/product';
 import { ActivatedRoute } from '@angular/router';
 import { ShoppingCartService } from '../shopping-cart.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit, OnDestroy {
-  cart: any;
+export class ProductsComponent implements OnInit {
+  cart$: Observable<ShoppingCart>;
   allProducts: Product [] = [];
   filteredProducts: Product [] = [];
   category: string;
-  subscription: Subscription;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute, private shoppingCartService: ShoppingCartService) {}
 
   async ngOnInit() {
-    this.subscription = (await this.shoppingCartService.getCart())
-    .subscribe(cart => this.cart = cart);
+    this.cart$ = await this.shoppingCartService.getCart();
+    this.populateProducts();    
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+  private populateProducts(){
+    let x: ShoppingCart;
+  
 
-  constructor(private productService: ProductService, private route: ActivatedRoute, private shoppingCartService: ShoppingCartService) 
-  {
+
     this.productService.getAll()
     .subscribe(
       query => {
-        query.docChanges().map(
+        query.docChanges()
+        .map(
           doc => {
             let product = doc.doc.data() as Product;
             product.uid = doc.doc.id;
 
             this.allProducts.push(product);
 
-            route.queryParamMap.subscribe(params => {
+            this.route.queryParamMap.subscribe(params => {
               this.category = params.get('category');
-              this.filteredProducts = (this.category) ?
-                this.allProducts.filter(p => p.category === this.category) : 
-                this.allProducts; 
+               this.applyFilter();
             });
           }
         )
       }
-    )  
+    ) 
   }
+
+  private applyFilter(){
+    this.filteredProducts = (this.category) ?
+                this.allProducts.filter(p => p.category === this.category) : 
+                this.allProducts;
+  }
+
+
+  
 
 }
