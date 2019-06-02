faffb177e5fa8c4ee8ef4fb8d39d69b7432763c1
Teltov, Kostiantyn (ext) <kostiantyn.teltov.ext@dematic.com>
2019-06-02 10:54:46 +0300

    Shopping cart component was added

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,11 +1,20 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart {
+    itemsArray: ShoppingCartItem[] = []
     constructor(public items: { [productId: string]: ShoppingCartItem }) {
         for(let productId in items){
             let item = items[productId];
+            this.itemsArray.push(new ShoppingCartItem(item.product, item.quantity));
         }           
-    }   
+    }
+    
+    get totalPrice(){
+        let sum = 0;
+        for(let productId in this.itemsArray)
+            sum += this.itemsArray[productId].totalPrice;
+        return sum;
+    }
 
     get totalItemsCount(){
         let count = 0;
